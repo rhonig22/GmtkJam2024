32ced33ae6b76579566140c70d5792287879ca58
rhonig22 <ryan.honig22@outlook.com>
2024-08-16 13:13:36 -0700

    Board Generation and Basic Movement
    - generate a board of light and dark squares
    - move the player orthogonally at discrete intervals

--- GmtkJam2024/Assets/Scripts/BoardGenerator.cs
+++ GmtkJam2024/Assets/Scripts/BoardGenerator.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class BoardGenerator : MonoBehaviour
 {
+    [SerializeField] GameObject _lightSquare;
+    [SerializeField] GameObject _darkSquare;
+    [SerializeField] int _width;
+    [SerializeField] int _height;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateBoard();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateBoard()
     {
-        
+        var startLight = true;
+        var iOffset = Mathf.FloorToInt(_width / 2);
+        var jOffset = Mathf.FloorToInt(_height / 2);
+        for (var i = 0; i < _width; i++)
+        {
+            var isLight = startLight;
+            for (var j = 0; j < _height; j++)
+            {
+                var square = Instantiate(isLight ?  _lightSquare : _darkSquare, transform);
+                square.transform.position = new Vector3(i - iOffset, j - jOffset, 0);
+                isLight = !isLight;
+            }
+
+            startLight = !startLight;
+        }
     }
 }
